4fce4ecb8fce354f88c469d1afc0ad97f89d0565
Andres Guerra <132696943+Andres97x@users.noreply.github.com>
2024-11-19 14:24:03 -0500

    added new projects and modified some others

--- projectsData.ts
+++ projectsData.ts
@@ -6,6 +6,22 @@ import image5 from './src/assets/project-5.jpg';
 import image6 from './src/assets/project-6.jpg';
 import image7 from './src/assets/project-7.jpg';
 import image8 from './src/assets/project-8.jpg';
+import image9 from './src/assets/project-9.jpg';
+import image10 from './src/assets/project-10.jpg';
+import image11 from './src/assets/project-11.jpg';
+import image12 from './src/assets/project-12.jpg';
+import image13 from './src/assets/project-13.jpg';
+import image14 from './src/assets/project-14.jpg';
+import image15 from './src/assets/project-15.jpg';
+import image16 from './src/assets/project-16.jpg';
+import image17 from './src/assets/project-17.jpg';
+import image18 from './src/assets/project-18.jpg';
+import image19 from './src/assets/project-19.jpg';
+import image20 from './src/assets/project-20.jpg';
+import image21 from './src/assets/project-21.jpg';
+import image22 from './src/assets/project-22.jpg';
+import image23 from './src/assets/project-23.jpg';
+import image24 from './src/assets/project-24.jpg';
 
 export default [
   {
@@ -55,5 +71,101 @@ export default [
     name: 'Centro del adulto mayor de Dabeiba',
     location: '',
     imageSrc: image8,
+  },
+  {
+    id: 9,
+    name: 'Estación de Bomberos de Chinú',
+    location: '',
+    imageSrc: image9,
+  },
+  {
+    id: 10,
+    name: 'Camu del Olivo',
+    location: '',
+    imageSrc: image10,
+  },
+  {
+    id: 11,
+    name: 'Iglesia Monteria',
+    location: '',
+    imageSrc: image11,
+  },
+  {
+    id: 12,
+    name: 'Sede del concejo Buenavista',
+    location: '',
+    imageSrc: image12,
+  },
+  {
+    id: 13,
+    name: 'Chancha de Futbol Chimá',
+    location: '',
+    imageSrc: image13,
+  },
+  {
+    id: 14,
+    name: 'Polideportivo Tierralta',
+    location: '',
+    imageSrc: image14,
+  },
+  {
+    id: 15,
+    name: 'Bodega LUYMA en Monteria',
+    location: '',
+    imageSrc: image15,
+  },
+  {
+    id: 16,
+    name: 'Estación elevadora Puerto Libertador',
+    location: '',
+    imageSrc: image16,
+  },
+  {
+    id: 17,
+    name: 'Casa Vallejo Monteria',
+    location: '',
+    imageSrc: image17,
+  },
+  {
+    id: 18,
+    name: 'Cubierta para parque infantil Los Cordobas',
+    location: '',
+    imageSrc: image18,
+  },
+  {
+    id: 19,
+    name: 'Estructura para tanques elevados Cáceres',
+    location: '',
+    imageSrc: image19,
+  },
+  {
+    id: 20,
+    name: 'Casa Avilez',
+    location: '',
+    imageSrc: image20,
+  },
+  {
+    id: 21,
+    name: 'Casa Jalisco',
+    location: '',
+    imageSrc: image21,
+  },
+  {
+    id: 22,
+    name: 'Mercado Público Purisima',
+    location: '',
+    imageSrc: image22,
+  },
+  {
+    id: 23,
+    name: 'PSFV Tierra Linda Chinú',
+    location: '',
+    imageSrc: image23,
+  },
+  {
+    id: 24,
+    name: 'Casa Pontevedra Monteria',
+    location: '',
+    imageSrc: image24,
   },
 ];
