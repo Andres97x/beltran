d78611fbd592b892ea4d0dc926a1112f6961f1a2
Andres Guerra <132696943+Andres97x@users.noreply.github.com>
2024-12-01 15:04:49 -0500

    finished form logic, styles, states and error handling

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,17 +1,44 @@
-import { FC, useState } from 'react';
+import { FC, useState, useRef } from 'react';
 import { IconType } from 'react-icons';
 import { HiOutlinePhone, HiOutlineLocationMarker } from 'react-icons/hi';
 import { MdOutlineTimer } from 'react-icons/md';
+import { LuCheckCircle } from 'react-icons/lu';
 import './contact.css';
 
 const Contact: FC = () => {
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const [error, setError] = useState<string | null>(null);
+  const [submitting, setIsSubmitting] = useState(false);
+  const [status, setStatus] = useState<'idle' | 'submitted'>('idle');
+  const nameRef = useRef<null | HTMLInputElement>(null);
+  const emailRef = useRef<null | HTMLInputElement>(null);
+  const messageRef = useRef<null | HTMLTextAreaElement>(null);
+
+  async function onSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    // checks
+    if (
+      !nameRef.current?.value.trim() ||
+      !emailRef.current?.value.trim() ||
+      !messageRef.current?.value.trim()
+    ) {
+      setError('Por favor completa los campos faltantes');
+      return;
+    }
+
+    if (emailRef.current?.value.trim()) {
+      const hasAtSymbol = /@/;
+      if (!hasAtSymbol.test(emailRef.current.value)) {
+        setError('Por favor usa un email válido');
+        return;
+      }
+    }
+
+    // send
     const formElement = event.target as HTMLFormElement;
     const formData = new FormData(formElement);
-
     formData.append('access_key', 'c9c7b58b-d058-47b5-adc4-6997a7f3581a');
 
+    setIsSubmitting(true);
     const response = await fetch('https://api.web3forms.com/submit', {
       method: 'POST',
       body: formData,
@@ -21,11 +48,18 @@ const Contact: FC = () => {
 
     if (data.success) {
       formElement.reset();
-      console.log('form submitted');
+      setIsSubmitting(false);
+      setStatus('submitted');
+
+      setTimeout(() => {
+        setStatus('idle');
+      }, 3000);
+      setError(null);
     } else {
-      console.error('Error', data);
+      setError('Hubo un error al enviar el email.');
+      setIsSubmitting(false);
     }
-  };
+  }
 
   return (
     <div className='contact'>
@@ -50,77 +84,92 @@ const Contact: FC = () => {
         />
       </div>
 
-      <div className='contact-form-container'>
-        <div className='contact-form-heading'>
-          <span className='sub-heading'>Contáctanos</span>
-          <h2 className='h2'>Listo para empezar</h2>
-          <p>
-            Háblanos sobre tus ideas y dejemos que nuestro equipo te ayude a dar
-            el primer paso hacia el éxito.
-          </p>
+      {status === 'idle' ? (
+        <div className='contact-form-container'>
+          <div className='contact-form-heading'>
+            <span className='sub-heading'>Contáctanos</span>
+            <h2 className='h2'>Listo para empezar</h2>
+            <p>
+              Háblanos sobre tus ideas y dejemos que nuestro equipo te ayude a
+              dar el primer paso hacia el éxito.
+            </p>
+          </div>
+          <form className='contact-form' onSubmit={onSubmit}>
+            <div>
+              <label htmlFor='nombre'>Nombre</label>
+              <input
+                ref={nameRef}
+                required
+                type='text'
+                name='nombre'
+                id='nombre'
+                placeholder='Juan Pérez'
+              />
+            </div>
+
+            <div>
+              <label htmlFor='email'>Email</label>
+              <input
+                ref={emailRef}
+                required
+                type='email'
+                name='email'
+                id='email'
+                placeholder='juan@testmail.com'
+                autoComplete='email'
+              />
+            </div>
+
+            <div>
+              <label htmlFor='telefono'>
+                Teléfono <span>opcional</span>
+              </label>
+              <input
+                type='text'
+                inputMode='numeric'
+                name='telefono'
+                id='telefono'
+                placeholder='312345678'
+              />
+            </div>
+
+            <div>
+              <label htmlFor='asunto'>
+                Asunto <span>opcional</span>
+              </label>
+              <input
+                type='text'
+                name='asunto'
+                id='asunto'
+                placeholder='Diseño estructural'
+              />
+            </div>
+
+            <div className='message-input-container'>
+              <label htmlFor='mensaje'>Mensaje</label>
+              <textarea
+                ref={messageRef}
+                required
+                name='mensaje'
+                id='mensaje'
+                placeholder='Escribe tu mensaje'
+              />
+            </div>
+
+            {error && (
+              <p className='error-message contact-error'>
+                Por favor completa los campos obligatorios
+              </p>
+            )}
+
+            <button disabled={submitting}>
+              {submitting ? 'Enviando...' : 'Enviar mensaje'}
+            </button>
+          </form>
         </div>
-        <form className='contact-form' onSubmit={onSubmit}>
-          <div>
-            <label htmlFor='nombre'>Nombre</label>
-            <input
-              required
-              type='text'
-              name='nombre'
-              id='nombre'
-              placeholder='Juan Pérez'
-            />
-          </div>
-
-          <div>
-            <label htmlFor='email'>Email</label>
-            <input
-              required
-              type='email'
-              name='email'
-              id='email'
-              placeholder='juan@testmail.com'
-              autoComplete='email'
-            />
-          </div>
-
-          <div>
-            <label htmlFor='telefono'>
-              Teléfono <span>opcional</span>
-            </label>
-            <input
-              type='text'
-              inputMode='numeric'
-              name='telefono'
-              id='telefono'
-              placeholder='312345678'
-            />
-          </div>
-
-          <div>
-            <label htmlFor='asunto'>
-              Asunto <span>opcional</span>
-            </label>
-            <input
-              type='text'
-              name='asunto'
-              id='asunto'
-              placeholder='Diseño estructural'
-            />
-          </div>
-
-          <div className='message-input-container'>
-            <label htmlFor='mensaje'>Mensaje</label>
-            <textarea
-              required
-              name='mensaje'
-              id='mensaje'
-              placeholder='Escribe tu mensaje'
-            />
-          </div>
-
-          <button>Enviar mensaje</button>
-        </form>
-      </div>
+      ) : (
+        <SubmittedView />
+      )}
     </div>
   );
 };
@@ -147,6 +196,22 @@ function ContactCard({
         <h6>{cardTitle}</h6>
         <span>{firstTextSnippet}</span>
         <span>{secondTextSnippet}</span>
+      </div>
+    </div>
+  );
+}
+
+function SubmittedView() {
+  return (
+    <div className='submitted-view'>
+      <div>
+        <div className='submitted-icon'>
+          <LuCheckCircle />
+        </div>
+        <div className='submitted-message'>
+          <p>Gracias!</p>
+          <span>Tu mensaje ha sido enviado.</span>
+        </div>
       </div>
     </div>
   );
